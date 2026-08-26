8c737fc321acfd39641e165bd82e9ed6b38a4fc7
Guilherme Pombo <gpombotky@gmail.com>
2026-08-26 13:47:43 -0300

    feat(SCV-58): implementa UI de inserir token

--- app/src/main/java/com/avlcode/scv/ui/theme/Color.kt
+++ app/src/main/java/com/avlcode/scv/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.avlcode.scv.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundColor = Color(0xFF1F2C34)
+val CardColor = Color(0xFF2A3942)
+val ButtonColor = Color(0xFF00A884)
+val WhiteColor = Color(0xFFFFFFFF)
